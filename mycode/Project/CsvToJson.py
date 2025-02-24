01c67be3a2c396b75f98354a50069a3963a39ead
oriraccoon <djdjskskalal@gmail.com>
2025-02-24 12:28:02 +0900

    asd

--- mycode/Project/CsvToJson.py
+++ mycode/Project/CsvToJson.py
@@ -12,6 +12,15 @@ def format_date(date_string):
         return f"{year}-{month}-{day}"  # "YYYY-MM-DD" 형식
     return None
 
+def format_apply(date_string):
+    match = re.match(r'(\d{4}).(\d{1,2}).(\d{1,2})', date_string)
+    if match:
+        year = match.group(1)
+        month = str(int(match.group(2))).zfill(2)  # 두 자릿수로 월 만들기
+        day = str(int(match.group(3))).zfill(2)    # 두 자릿수로 일 만들기
+        return f"{year}-{month}-{day}"  # "YYYY-MM-DD" 형식
+    return None
+
 # CSV를 JSON으로 변환하는 함수
 def csvTojson():
     # CSV 파일 경로
@@ -28,6 +37,8 @@ def csvTojson():
     for concert in data:
         if '예매날짜' in concert:
             concert['예매날짜'] = format_date(concert['예매날짜'])
+        if '등록일' in concert:
+            concert['등록일'] = format_apply(concert['등록일'])
 
     # JSON 파일로 저장
     with open(json_file, mode='w', encoding='utf-8') as file:
